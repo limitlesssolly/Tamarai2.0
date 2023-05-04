f42124d3797b260fdf2c68cacd9936cfda316936
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-04 14:45:25 +0300

    Initializing express.js

--- JS/Server.js
+++ JS/Server.js
@@ -3,3 +3,6 @@ const express = require('express');
 const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
+
+// Initializing express.js
+const app = express();
