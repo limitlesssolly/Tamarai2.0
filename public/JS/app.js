2443f3e5d4b2aad99c8a18a7015cce5e6672c535
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-05 12:56:44 +0300

    Update app.js

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,8 +1,16 @@
 // Importing packages
 const express = require('express');
-const admin = require('firebase-admin');
-const bcrypt = require('bcrypt');
 const path = require('path');
+
+const hostname = "localhost";
+const port = 8080;
 
 // Initializing express.js
 const app = express();
+
+// Home page
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port);
