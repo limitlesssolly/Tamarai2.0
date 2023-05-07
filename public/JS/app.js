60a9cd7157476beb5fede9fbc5e355c402502c72
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-07 23:20:11 +0300

    npm install express

--- public/JS/app.js
+++ public/JS/app.js
@@ -2,15 +2,15 @@
 const express = require('express');
 const path = require('path');
 
-const hostname = "localhost";
+const hostname = 'localhost';
 const port = 8080;
 
 // Initializing express.js
 const app = express();
 
 // Home page
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port);
