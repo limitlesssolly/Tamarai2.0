84d37d0048ace853d874dbe69d88e02d83da6cf5
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-10 11:42:55 +0300

    Remove JS

--- public/JS/Server.js
+++
@@ -1,8 +0,0 @@
-// Importing packages
-const express = require('express');
-const admin = require('firebase-admin');
-const bcrypt = require('bcrypt');
-const path = require('path');
-
-// Initializing express.js
-const app = express();
