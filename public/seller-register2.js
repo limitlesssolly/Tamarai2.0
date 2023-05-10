5cecb8717e52f61e273dc7c1f7151295e3cd1b70
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-10 11:44:50 +0300

    Remove kolo except readme file

--- public/seller-register2.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const seller = express();
-seller.get('/',(req,res)=>{
-    res.send("welcome to tamarai");
-});
-seller.listen(4000,()=>{
- console.log("listining to port no 4000");
-});
