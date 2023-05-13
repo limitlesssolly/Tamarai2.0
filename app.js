38f314e7b4ce74ead0aa847a84d1bea220ed174c
solsol <shahdahmed7203@gmail.com>
2023-05-13 20:51:05 +0300

    m3rfsh

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('Hello World');
+//const file = require('./filename)
+//module.export= what is gonna b returned to the file that requires it (could b an object full of properties)
+//const {people} = require('./people) bakhod el object 3latool bas lazm yb2o same name
+//readfile(relativepath, text to write, call back function ()=>{}) zai readfile bas de zyada feha el haytktb fl file
