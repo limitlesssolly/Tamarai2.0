95f8ce2c2e6b7e7e557761b1e6e93f90352d7214
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-13 22:45:32 +0300

    app.js makes error

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-import HttpError from "http-errors";
-import express from 'express';
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import { fileURLToPath } from "url";
-import expressLayouts from 'express-ejs-layouts';
+// import HttpError from "http-errors";
+// import express from 'express';
+// import path from "path";
+// import cookieParser from "cookie-parser";
+// import logger from "morgan";
+// import { fileURLToPath } from "url";
+// import expressLayouts from 'express-ejs-layouts';
 
-const app = express();
+// const app = express();
 
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
+// export const __filename = fileURLToPath(import.meta.url);
+// export const __dirname = path.dirname(__filename);
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'public')));
-app.use('/', router);
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname,'public')));
+// app.use('/', router);
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
     
-    // render the error page
-    res.status(err.status || 500);
-    res.render('pages/error');
-  });
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('pages/error');
+//   });
 
 
 
-export default app;
+// export default app;
