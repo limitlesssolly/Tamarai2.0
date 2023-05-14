b6443a032717175729a630a0848e0bc8e3217a52
solsol <shahdahmed7203@gmail.com>
2023-05-14 20:35:22 +0300

    more files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
     res.render('index.ejs');
 });
 
+router.get('/sign-in', function(req,res,next){
+    res.render('user-sign-in.ejs');
+});
+
+router.get('/sign-up', function(req,res,next){
+    res.render('user-register.ejs');
+});
 export default router;
