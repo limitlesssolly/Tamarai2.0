430f41f8b6675b75c46352fde0925fc04a07bae4
solsol <shahdahmed7203@gmail.com>
2023-05-17 15:00:51 +0300

    more upds

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,16 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-router.get('/index.ejs', function (req, res, next){
-    res.render('index');
-})
-
 router.get('/admin-sign-in', function (req, res, next){
     res.render('Admin/admin-sign-in');
+})
+
+router.get('/user-sign-in.ejs', function (req, res, next){
+    res.render('User/user-sign-in');
+})
+
+router.get('/user-register.ejs', function (req, res, next){
+    res.render('User/user-register');
 })
 
 export default router;
