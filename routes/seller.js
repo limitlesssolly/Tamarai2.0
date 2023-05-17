e75635f0f0e6555010667f651149aeb2da882586
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-05-18 00:12:25 +0300

    ejs

--- routes/seller.js
+++ routes/seller.js
@@ -1,10 +1,31 @@
-import { Router } from 'express';
-const router = Router();
+import express from 'express';
+import Seller from '../models/sellerData.js';
 
-router.get('/seller', function(req, res, next) {
-    // res.send('she works!');
-    res.render('seller-sign-in.ejs');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('seller-sign-in', { seller: new Seller() });
 });
 
+router.post('/', async (req, res) => {
+  const seller = new Seller({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  try {
+    const newSeller = await seller.save();
+    res.redirect('/seller/dashboard');
+  } catch (err) {
+    res.render('seller-sign-in', {
+      seller: seller,
+      errorMessage: 'Error signing up',
+    });
+  }
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('seller-dashboard');
+});
 
 export default router;
