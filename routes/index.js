34cf9d9effc7271d0f4d3005def8fbd992d5c3a2
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-05-18 13:34:09 +0300

    removing ejs

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,14 @@ router.get('/admin-sign-in', function (req, res, next){
     res.render('Admin/admin-sign-in');
 })
 
-router.get('/user-sign-in.ejs', function (req, res, next){
+router.get('/user-sign-in', function (req, res, next){
     res.render('User/user-sign-in');
 })
 
-router.get('/user-register.ejs', function (req, res, next){
+router.get('/user-register', function (req, res, next){
     res.render('User/user-register');
 })
-router.get('/seller', function (req, res, next){
+router.get('/seller-sign-in', function (req, res, next){
     res.render('seller/seller-sign-in');
 })
 export default router;
