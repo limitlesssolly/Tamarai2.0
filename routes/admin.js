2d166f6e8da6916b6dfcd8c1e661210ad4aaea9e
solsol <shahdahmed7203@gmail.com>
2023-05-18 17:31:33 +0300

    admin data works

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-// import  Admin  from '../models/adminData.js';
+import  Admin  from '../models/adminData.js';
 
 /* GET /admin page. */
 router.get('/', function (req, res, next){
@@ -9,35 +9,35 @@ router.get('/', function (req, res, next){
 
 /* GET /admin/dashboard page. */
 router.get('/dashboard', function (req, res, next){
-    res.render('');
+    res.render('admin/admin-dashboard.ejs');
 })
 
-// router.post('/', async function(req, res, next)
-// {
-//     const admin = new Admin({
-//         username: req.body.name,
-//         password: req.body.pass,
-//     })
-//     admin.save()
-//     .then((result)=>
-//     {
-//         res.render('admin-dashboard')
-//     })
-//     .catch(err=>{
-//         console.log(err);
-//     })
-//     // try
-//     // {
-//     //     const newAdmin = await admin.save();
-//     //     // res.redirect(`admin/$newAdmin.username`)
-//     //     res.redirect('admin-dashboard');
-//     // }
-//     // catch
-//     // {
-//     //     res.render('admin-sign-in',{
-//     //         admin: admin,
-//     //         errorMessage :'error signing up'
-//     //     })
-//     // }
-// })
+router.post('/', async function(req, res, next)
+{
+    const admin = new Admin({
+        username: req.body.name,
+        password: req.body.pass,
+    })
+    admin.save()
+    .then((result)=>
+    {
+        res.render('admin/admin-dashboard.ejs')
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+    // try
+    // {
+    //     const newAdmin = await admin.save();
+    //     // res.redirect(`admin/$newAdmin.username`)
+    //     res.redirect('admin-dashboard');
+    // }
+    // catch
+    // {
+    //     res.render('admin-sign-in',{
+    //         admin: admin,
+    //         errorMessage :'error signing up'
+    //     })
+    // }
+})
 export default router;
