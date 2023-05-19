c1815c699de2d1e2049e5ba74289b39fd3ecf9d2
Nadine <nadinehamdy128@gmail.com>
2023-05-19 19:09:35 +0300

    :)

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 const app = express();
+app.use(express.static('public'));
 
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import fileUpload from 'express-fileupload';
@@ -17,20 +18,21 @@ import sellerRouter from "./routes/seller.js";
 import userRouter from "./routes/user.js"
 
 //Read the current directory name
-export const __filename = fileURLToPath(import.meta.url);
+export const __filename = fileURLToPath(
+    import.meta.url);
 export const __dirname = path.dirname(__filename);
 console.log(`Project Root dir : ${__dirname}`);
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));// to join th static folder "views" which contains the ejs files so that it can run 
-app.set("view engine", "ejs");// first thin we do when using ejs 
+app.set("views", path.join(__dirname, "views")); // to join th static folder "views" which contains the ejs files so that it can run 
+app.set("view engine", "ejs"); // first thin we do when using ejs 
 
 app.use(logger("dev"));
 app.use(fileUpload());
 // app.use(session({ secret: 'Your_Secret_Key' }))
 app.use(express.json());
-app.use(bodyParser.urlencoded({limit: '10mb',extended: false}));
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -44,8 +46,8 @@ app.use('/admin/dashboard', adminDashboardRouter);
 app.use('/seller', sellerRouter);
 app.use('/user', userRouter);
 
- // error handler
-app.use(function (err, req, res, next) {
+// error handler
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -55,10 +57,10 @@ app.use(function (err, req, res, next) {
     res.render('error.ejs');
 });
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index'));
 });
-  
+
 app.set('port', process.env.PORT || 7777);
 
 export default app;
