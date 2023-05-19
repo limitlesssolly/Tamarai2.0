efccf5dfd33ad4eaa4f66b291a41260cca336cae
Nadine <nadinehamdy128@gmail.com>
2023-05-19 14:41:32 +0300

    Revert "linking try"
    This reverts commit bcc4268fb099f0a2d7d71faf547e6cd7ccc77ebd.

--- models/adminData.js
+++ models/adminData.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
 const Admin = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
+    username:{
+        type:String,
+        required:true,
     },
-    password: {
-        type: String,
-        required: true,
+    password:{
+        type:String,
+        required:true,
     },
 }, { timestamps: true })
 
