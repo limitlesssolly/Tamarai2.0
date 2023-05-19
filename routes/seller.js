b773b75ce4a31c9a4ab0f799b1237580037e5f4b
Nadine <nadinehamdy128@gmail.com>
2023-05-19 15:15:37 +0300

    hope ded

--- routes/seller.js
+++ routes/seller.js
@@ -1,35 +1,39 @@
-import {Router} from 'express';
+import { Router } from 'express';
 const router = Router();
 
 /* GET /seller page. */
 router.get('/', (req, res) => {
-  res.render('seller-sign-in', { seller: new Seller() });
+    res.render('seller-sign-in', { seller: new Seller() });
 });
 
 /* GET /seller/register page. */
 router.get('/register', (req, res) => {
-  res.render('seller-register');
+    res.render('seller-register');
 });
 
-router.post('/', async (req, res) => {
-  const seller = new Seller({
-    username: req.body.username,
-    password: req.body.password,
-  });
-
-  try {
-    const newSeller = await seller.save();
-    res.redirect('/seller/dashboard');
-  } catch (err) {
-    res.render('seller-sign-in', {
-      seller: seller,
-      errorMessage: 'Error signing up',
+router.post('/', async(req, res) => {
+    const seller = new Seller({
+        username: req.body.username,
+        password: req.body.password,
     });
-  }
+
+    try {
+        const newSeller = await seller.save();
+        res.redirect('/seller/dashboard');
+    } catch (err) {
+        res.render('seller-sign-in', {
+            seller: seller,
+            errorMessage: 'Error signing up',
+        });
+    }
 });
 
 router.get('/dashboard', (req, res) => {
-  res.render('seller-dashboard');
+    res.render('seller-dashboard');
+});
+
+router.get('/seller/seller-info.ejs', (req, res) => {
+    res.render('seller-info');
 });
 
 export default router;
