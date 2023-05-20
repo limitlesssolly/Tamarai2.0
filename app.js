9b90565886faa217d69e938ea15c2c42731d8d09
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-20 14:27:16 +0300

    remove repitition

--- app.js
+++ app.js
@@ -47,17 +47,6 @@ app.use('/admin/dashboard', adminDashboardRouter);
 app.use('/seller', sellerRouter);
 app.use('/user', userRouter);
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error.ejs');
-});
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index'));
 });
