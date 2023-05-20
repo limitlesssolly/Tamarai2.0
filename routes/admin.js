89c15dec1bac0e0c45f0f6ce4fdfa2ea812d7fb4
solsol <shahdahmed7203@gmail.com>
2023-05-20 15:28:12 +0300

    sal7t login bgd

--- routes/admin.js
+++ routes/admin.js
@@ -12,22 +12,28 @@ router.post('/', async function(req, res, next)
 {
     var un = req.body.name;
     var pw = req.body.pass;
-    const admins = await admin.findOne({ username: un });
-    console.log(admins)
 
-    if (admins === null)
+    // admin.find({}, function(err, admin){
+    //     res.render('AdminDatabase', { admin : admin});
+    //    });
+
+    const admins = await admin.find({});
+    console.log(admins)
+    var i;
+    for (i = 0; i< admins.length; i++)
     {
-        // document.getElementById('Uerror').innerHTML = 'Enter a valid username';
-        // document.getElementById('Uerror').style.display = 'block';
-        console.log("fe mashakel")
-        res.render('error.ejs')
-    }
-    else
-    {
-        if (admin.findOne({ password: pw }))
+        if (admins[i].username === un)
         {
-            console.log("login successful!")
-            res.render('admin/admin-dashboard.ejs', {name : req.body.name})
+            if (admins[i].password === pw)
+            {
+                console.log("login successful!")
+                res.render('admin/admin-dashboard.ejs', {name : req.body.name})
+            }
+            else
+            {
+                console.log("fe mashakel")
+                res.render('error.ejs')
+            }
         }
         else
         {
@@ -35,6 +41,28 @@ router.post('/', async function(req, res, next)
             res.render('error.ejs')
         }
     }
+    // const admins = await admin.findOne({ username: un });
+    // console.log(admins)
+
+    // if (admins === null)
+    // {
+    //     // document.getElementById('Uerror').innerHTML = 'Enter a valid username';
+    //     // document.getElementById('Uerror').style.display = 'block';
+    //     console.log("fe mashakel")
+    //     res.render('error.ejs')
+    // }
+    // else
+    // {
+    //     if (admin.findOne({ password: pw }))
+    //     {
+    //         console.log("login successful!")
+    //         res.render('admin/admin-dashboard.ejs', {name : req.body.name})
+    //     }
+    //     else
+    //     {
+            
+    //     }
+    // }
 })
 
 // const hashPass = await bcrypt.hash(req.body.pass, 10)  di 3shan nsave el pass hashed fl sign up
