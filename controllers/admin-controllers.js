18870eefe64bc4bd3308cef4fc140b5c7d4930e4
solsol <shahdahmed7203@gmail.com>
2023-05-22 21:10:02 +0300

    admin tings

--- controllers/admin-controllers.js
+++ controllers/admin-controllers.js
@@ -1,6 +1,34 @@
 import { body, validationResult } from "express-validator";
 import admin from '../models/adminData.js';
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
+
+// const validateSignup = [
+//   body("username").notEmpty().withMessage("Username is required"),
+//   body("password")
+//     .isLength({ min: 6 })
+//     .withMessage("Password must be at least 6 characters"),
+//   body("password")
+//     .matches(
+//       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/
+//     )
+//     .withMessage(
+//       "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character"
+//     ),
+//   body("confirmPassword")
+//     .custom((value, { req }) => value === req.body.password)
+//     .withMessage("Passwords do not match"),
+// ];
+
+// const signupController = (req, res) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     res.render("index", {
+//       errors: errors.array(),
+//     });
+//   } else {
+//     res.send("Signup successful");
+//   }
+// };
 
 const signins = async (req, res, next) => {
   var un = req.body.name;
@@ -32,8 +60,8 @@ const signups = async (req, res, next) => {
   // const hashPass = await bcrypt.hash(req.body.pass, 10)
 
   const newadmin = new admin({
-    username: req.body.name,
-    password: req.body.pass,
+    username: req.body.username,
+    password: req.body.password,
 })
 newadmin.save()
 .then((result)=>
--- routes/adminDashboard.js
+++ routes/adminDashboard.js
@@ -6,5 +6,35 @@ router.get('/', function (req, res, next){
     res.render('admin/admin-dashboard');
 })
 
+/* GET /admin/dashboard/stats page. */
+router.get('/stats', function (req, res, next){
+    res.render('admin/admin-stats');
+})
+
+/* GET /admin/dashboard/finances page. */
+router.get('/finances', function (req, res, next){
+    res.render('admin/admin-finances');
+})
+
+/* GET /admin/dashboard/messages page. */
+router.get('/messages', function (req, res, next){
+    res.render('admin/admin-messages');
+})
+
+/* GET /admin/dashboard/sellings page. */
+router.get('/sellings', function (req, res, next){
+    res.render('admin/admin-sellings');
+})
+
+/* GET /admin/dashboard/usings page. */
+router.get('/usings', function (req, res, next){
+    res.render('admin/admin-usings');
+})
+
+/* GET /admin/dashboard/settings page. */
+router.get('/settings', function (req, res, next){
+    res.render('admin/admin-settings');
+})
+
 
 export default router;
