0a35f814b0faed6de45c83c77aeb168f16f9de29
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-25 21:57:33 +0300

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,22 @@
 import { Router } from 'express';
 const router = Router();
+// MONGODB
+const MongoClient = require('mongodb').MongoClient;
+// Initialize MongoDB Client
+const client = new MongoClient(process.env.ATLAS_URI, { useUnifiedTopology: true });
+//
+
 
 // Get products page
-router.get('/', function (req, res, next){
+router.get('/', (req, res) => {
+    // res.send("hello");   // الوحيدة اللي شغاله
     // res.render('user/product');
-    res.send("hello");
-})
+    // Retrieve products data from MongoDB
+    Db.collection('products').find().toArray((err, products) => {
+        if (err) throw err;
+        // send product data to EJS template
+        res.render('products', { products });
+    });
+});
 
 export default router;
