66ec7585b68d5bb9af20fb09d86b556f45d28b4b
Nadine <nadinehamdy128@gmail.com>
2023-05-25 13:27:00 +0300

    Revert "xnnc jksbvdf"
    This reverts commit eb792b642fbad3f4c9b1975c72c8eb8ef4e5964b.

--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,9 @@ import { Router } from 'express';
 const router = Router();
 
 // Get products page
-router.get('/', function(req, res, next) {
-    /* res.render('user/product');*/
-    res.send("hello");
+router.get('/', function (req, res, next){
+    res.render('user/product');
+    // res.send("hello");
 })
 
 export default router;
