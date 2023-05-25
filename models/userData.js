213c91397f4bf5dc459903f896adae3fcfdde971
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-05-25 18:09:07 +0300

    User's Data

---
+++ models/userData.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    username : {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    whishlist: {
+        type: Array,
+    },
+}, { timestamps: true });
+
+const Users = mongoose.model('usersData', userSchema);
+export default Users;
