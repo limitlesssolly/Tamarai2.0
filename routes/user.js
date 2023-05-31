76ed121f3431ebdd8f966b64398eefb0dc6f62c1
Nadine <nadinehamdy128@gmail.com>
2023-05-31 15:01:45 +0300

    linking shopping cart and testine env ignoring

--- routes/user.js
+++ routes/user.js
@@ -27,7 +27,9 @@ router.get('/homepage', function(req, res, next) {
 router.get('/homepage/checkout', function(req, res, next) {
     res.render('user/user-checkout');
 })
-
+router.get('/homepage/bag', function(req, res, next) {
+    res.render('user/user-shoppingbag');
+})
 
 router.post('/', signins);
 
