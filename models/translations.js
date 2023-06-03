183b77789290d1a7dfba2e95d3950c0f22b9de6a
Nadine <nadinehamdy128@gmail.com>
2023-06-04 00:34:38 +0300

    Revert "Revert "msh 3arfa a3mel localization""
    This reverts commit 1de797702571e04824a8d857851d58d5351bfc73.

---
+++ models/translations.js
@@ -0,0 +1,35 @@
+const translations = {
+    en: {
+      TITLE: 'Tamarai - Buy and Sell Art Online',
+      NAV_HOME: 'Home',
+      NAV_SERVICES: 'Services',
+      NAV_FAV: 'Favorites',
+      NAV_PROFILE: 'Profile',
+      NAV_CART: 'Shopping Cart',
+      BTN_SIGN_IN: 'Sign In',
+      BTN_SIGN_UP: 'Sign Up',
+      BANNER_H1: 'TAMA<span>RAI</span>',
+      BANNER_H3: "If you can't beat Lot(us), <span> Join Lot(us)</span>",
+      BANNER_P: 'Tamarai is an e-commerce website for artistic ideas and art pieces that allows a user to purchase and sell at the same time! So, Don\'t miss this chance. Wanna Learn more about Tamarai?',
+      BTN_LEARN_MORE: 'Learn More',
+      BTN_ABOUT_US: 'About Us'
+    },
+    fr: {
+      TITLE: 'Tamarai - Achetez et vendez de l\'art en ligne',
+      NAV_HOME: 'Accueil',
+      NAV_SERVICES: 'Services',
+      NAV_FAV: 'Favoris',
+      NAV_PROFILE: 'Profil',
+      NAV_CART: 'Panier',
+      BTN_SIGN_IN: 'Se Connecter',
+      BTN_SIGN_UP: 'S\'inscrire',
+      BANNER_H1: 'TAMA<span>RAI</span>',
+      BANNER_H3: "Si vous ne pouvez pas battre Lot(us), <span>Rejoignez Lot(us)</span>",
+      BANNER_P: 'Tamarai est un site web de commerce électronique pour les idées artistiques et les pièces d\'art qui permet à un utilisateur d\'acheter et de vendre en même temps ! Alors, ne manquez pas cette chance. Vous voulez en savoir plus sur Tamarai?',
+      BTN_LEARN_MORE: 'En savoir plus',
+      BTN_ABOUT_US: 'À propos de nous'
+    }
+  };
+  
+  module.exports = translations;
+  
