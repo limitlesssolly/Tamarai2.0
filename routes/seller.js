f63a316fe61fbf91bf09167f8602702a54244a2c
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-06-03 08:35:21 +0300

    profile tany

--- routes/seller.js
+++ routes/seller.js
@@ -28,7 +28,7 @@ router.get('/info', function(req, res, next) {
 router.get('/products', function(req, res, next) {
     res.render('seller/seller-products');
 })
-
+/* GET /seller/profile page. */
 router.get('/profile', function(req, res, next) {
     res.render('seller/seller-profile');
 })
