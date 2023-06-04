2ea78771a00072748be161394bbdb7e4b5f8f55a
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2023-06-04 16:55:17 +0300

    2arbt awsal ya farag allah

--- routes/sellerDashboard.js
+++ routes/sellerDashboard.js
@@ -1,39 +1,45 @@
-import { Router } from 'express';
+import {
+    Router
+} from 'express';
 const router = Router();
 import Prod from '../models/productData.js';
-import {addItem} from "../controllers/seller-controller.js";
+import {
+    addItem
+} from "../controllers/seller-controller.js";
 import seller from "../models/sellerRegister.js";
 import regi from "../models/tryseller.js";
 
 /* GET /seller/dashboard page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('seller/seller-dashboard');
 })
 
 
 /* GET /seller/dashboard/add page. */
-router.get('/add', function(req, res, next) {
+router.get('/add', function (req, res, next) {
     res.render('seller/seller-add');
 });
 
 /* GET /seller/dashboard/view page. */
-router.get('/add', function(req, res, next) {
+router.get('/add', function (req, res, next) {
     res.render('seller/seller-add');
 });
 
 /* post an item */
-router.post('/add',addItem);
+router.post('/add', addItem);
 
 /* GET /seller/dashboard/products page. */
-router.get('/products', function(req, res, next) {
+router.get('/products', function (req, res, next) {
     res.render('seller/seller-products');
 });
 
 
 /* GET /seller/dashboard/view page. */
-router.get('/view',async function(req, res, next) {
+router.get('/view', async function (req, res, next) {
     const Products = await Prod.find();
-    res.render('seller/seller-view' , {Products});
+    res.render('seller/seller-view', {
+        Products
+    });
 })
 
 /* Delete One item using id */
@@ -45,20 +51,37 @@ router.get('/view/delete/:id', async function (req, res, next) {
 })
 
 /* GET /seller/dashboard/info page. */
-router.get('/info', function(req, res, next) {
+router.get('/info', function (req, res, next) {
     res.render('seller/seller-info');
 })
 
 /* GET /seller/dashboard/profile page. */
 router.get('/profile', async (req, res) => {
     const sellers = await regi.find();
-       res.render('seller/seller-profile', {sellers});
- });
+    res.render('seller/seller-profile', {
+        sellers
+    });
+});
 
 /* GET /seller/dashboard/profile page. */
  router.get('/profile/:id', async (req, res) => {
-      const regs = await regi.findById(req.params.id);
-       res.render('seller/seller-profile', {regs});
-   });
+    const regs = await regi.findById(req.params.id);
+    res.render('seller/seller-profile', {regs});
+ });
+
+
+ router.post('/profile/:id', async (req, res) => {
+    const regs = await regi.findById(req.params.id);
+    regs.username = req.body.username;
+    regs.email = req.body.email;
+    // regs.password = req.body.password;
+    await regs.save();
+
+    // Retrieve the updated seller data from the database
+    const updatedSell = await regi.findById(req.params.id);
+
+    // Render the "profile" view with the updated seller data
+    res.render('seller/seller-profile', { regs: updatedSell });
+});
 
 export default router;
