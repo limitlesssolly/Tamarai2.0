807d3ebbe41ca3dc4cc0a65399a97d3ee95ebe24
Minna-Abdelhady <minna2109122@miuegypt.edu.eg>
2023-06-05 14:41:30 +0300

    PRODUCTS

--- routes/product.js
+++ routes/product.js
@@ -4,16 +4,18 @@ import Product from "../models/productData.js";
 const router = Router();
 
 // Get all products /products
-router.get('/', (req, res, next) => {
-    // res.send("All products");
-    //Retrieve products data from MongoDB
-    const products = Product.find({}).then((products) => {
-        //  res.json(products);
-        // res.render('user/try', { products: products });  // NOT WORKING WITH PRODUCT
-        res.render('user/products', { products: products });
-    }).catch((err) => {
-        next(err);
-    });
+router.get('/', async (req, res, next) => {
+    // // res.send("All products");
+    // //Retrieve products data from MongoDB
+    // const products = Product.find({}).then((products) => {
+    //     //  res.json(products);
+    //     // res.render('user/try', { products: products });  // NOT WORKING WITH PRODUCT
+    //     res.render('user/products', { products });
+    // }).catch((err) => {
+    //     next(err);
+    // });
+    const products = await Product.find();
+    res.render('user/products', { products });
 });
 
 // Get a single product by its ID
